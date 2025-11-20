e7366a3a6e7d05d15780aed1e814a8ff5bbb8bc2
Pham Hong Thai <pthai9120@gmail.com>
2025-11-20 14:12:59 +0700

    add

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
-DB_NAME = os.getenv("DB_NAME", "fastapi_db")
+DB_NAME = os.getenv("DB_NAME", "")
 
 # Tạo connection string
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
